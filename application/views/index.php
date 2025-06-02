1fc694eb29dad41114f9951e03088b0d3cac4063
hikmalharun <hikmal.harun@gmail.com>
2025-06-02 10:13:58 +0700

    SPMB

--- application/views/index.php
+++ application/views/index.php
@@ -48,10 +48,9 @@
                     <i class="bi bi-telephone d-flex align-items-center ms-4"><span>(0234) 7150464</span></i>
                 </div>
                 <div class="social-links d-none d-md-flex align-items-center">
-                    <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
-                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    <a href="https://facebook.com/sman1sukra" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="https://instagram.com/sman1sukra" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="https://youtube.com/sman1sukra" class="youtube"><i class="bi bi-youtube"></i></a>
                 </div>
             </div>
         </div><!-- End Top Bar -->
